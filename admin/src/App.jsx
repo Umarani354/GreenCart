3efd3dc7d0e282888741feff3ef9e801dc58188b
Umarani A Bharamanaikar <umaranibharamanaikar@gmail.com>
2025-08-10 18:33:55 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = 'http://localhost:4000';
+  const url = 'https://greencart-backend-76yi.onrender.com';
 
   return (
     <div>
