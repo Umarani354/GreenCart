48cc92956b714c055bca4b3eaace20d93b679fc4
Umarani <umaranibharamanaikar@gmail.com>
2025-08-10 18:21:20 +0530

    first-commit

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,6 +1,6 @@
 import basket_icon from './basket_icon.png'
 import logo from './logo.png'
-import header_img from './header_img.png'
+// import header_img from '/images/header_img.png'
 import search_icon from './search_icon.png'
 import beverages from './beverages.jpg'
 import snacks from './snacks.png'
@@ -56,6 +56,8 @@ import profile_icon from './profile_icon.png'
 import bag_icon from './bag_icon.png'
 import logout_icon from './logout_icon.png'
 import parcel_icon from './parcel_icon.png'
+
+const header_img = '/images/header_img.png';
 
 export const assets = {
     logo,
