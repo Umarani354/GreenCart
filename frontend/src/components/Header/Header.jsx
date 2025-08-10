7d7be8e7e9b6a3ad706cba79a0a91bc9c34624ab
Umarani <umaranibharamanaikar@gmail.com>
2025-08-10 17:46:41 +0530

    first-commit

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div className='header'>
+    <div className='header' >
         <div className="header-contents">
             {/* <h2>Order your favourite organic food  here</h2> */}
             <button><a href="#explore-menu">View Menu</a></button>
